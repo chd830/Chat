d746321d6f86aef19957214ccd8e7d40f1f14c52
chong <chd830@naver.com>
2019-04-15 23:28:38 +0900

    js,css못읽는 오류 있음

--- app.js
+++ app.js
@@ -5,14 +5,27 @@ const socket = require('socket.io')
 
 const http = require('http')
 
+const fs = require('fs');
+
 const app = express()
 
 const server = http.createServer(app)
 
 const io = socket(server)
-
+app.use('/css', express.static('./static/css'))
+app.use('/js', express.static('./static/js'))
+/*외부클라이언트가 /css, /js경로로 액세스 가능*/
 
 app.get('/', function(request, response) {
+    fs.readFile('./static/index.html', function(err, data) {
+        if(err) {
+            response.send('error')
+        } else {
+            response.writeHead(200, { 'Content-Type': 'text/html' })
+            response.write(data)
+            response.end()
+        }
+    })
     console.log('user login')
     response.send('Hello Express Server')
 }) 

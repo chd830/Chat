fe4d0559f5f4e306f2df3d21519123a536421531
chong <chd830@naver.com>
2019-04-24 13:54:37 +0900

    namespace socket으로 나누기

--- app.js
+++ app.js
@@ -54,37 +54,75 @@
 ///* response.send로 데이터 보내기 */
 
 
+//const app = require('express')();
+//
+//const server = require('http').Server(app);
+//
+//const io = require('socket.io')(server);
+//
+//const fs = require('fs');
+//
+////io.on('connection', (socket) => {
+////    console.log('a user connected');
+////    
+////    socket.on('chat message', (msg) => {
+////        io.emit('chat message', msg);
+////    });
+////    socket.on('disconnect', () => {
+////        console.log('user disconnected');
+////    });
+////});
+//
+//app.get('/', function(request, response) {
+//    fs.readFile('./static/index.html', function(err, data) {
+//        if(err) {
+//            response.send('error')
+//        } else {
+//            response.writeHead(200, { 'Content-Type': 'text/html' })
+//            response.write(data)
+//            response.end()
+//        }
+//    })
+//});
+//server.listen(8080, () => {
+//    console.log('Connected at 8080');
+//});
+//
+//const namespace1 = io.of('/namespace1');
+//
+//namespace1.on('connection', (socket) => {
+//    namespace1.emit('new', {hello: "someone connected at namespace1"});
+//});
+//
+//const namespace2 = io.of('/namespace2');
+//
+//namespace2.on('connection', (socket) => {
+//    namespace2.emit('new', {hello: "someone connected at namespace2"});
+//});
 const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-const http = require('http').createServer(app);
-
-const io = require('socket.io')(http);
-
-const fs = require('fs');
-
-app.get('/', function(request, response) {
-    fs.readFile('./static/index.html', function(err, data) {
-        if(err) {
-            response.send('error')
-        } else {
-            response.writeHead(200, { 'Content-Type': 'text/html' })
-            response.write(data)
-            response.end()
-        }
-    })
+server.listen(3000, () => {
+  console.log('connect 3000');
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/static/index.html');
 });
 
-http.listen(8080, () => {
-    console.log('Connected at 8080');
+// NameSpace 1번
+const namespace1 = io.of('/namespace1');
+
+// connection을 받으면, news 이벤트에 hello 객체를 담아 보낸다
+namespace1.on('connection', (socket) => {
+  namespace1.emit('news', { hello: "Someone connected at namespace1" });
+});
+
+// NameSpace 2번
+const namespace2= io.of('/namespace2');
+
+// connection을 받으면, news 이벤트에 hello 객체를 담아 보낸다
+namespace1.on('connection', (socket) => {
+  namespace1.emit('news', { hello: "Someone connected at namespace2" });
 });
--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,16 @@
-
-
-io().on('connect', function() {
-    /* 접속 되었을 때 실행*/
-    var input = documnet.getElementById('test')
-    input.value = 'LogIn'
-})
-
-function send() {
-    var message = document.getElementById('test').value
-    /* 입력되어 있는 데이터 가져오기*/
-    document.getElementById('test').value = ''
-    
-    io().emit('send',{ msg : message})
-    /* 서버로 send 이벤트 전달(데이터포함)*/
-}
+//var socket = io();
+//
+//socket.on('connect', function() {
+//    /* 접속 되었을 때 실행*/
+//    var input = documnet.getElementById('test')
+//    input.value = 'LogIn'
+//})
+//
+//function send() {
+//    var message = document.getElementById('test').value
+//    /* 입력되어 있는 데이터 가져오기*/
+//    document.getElementById('test').value = ''
+//    
+//    socket.emit('send',{ msg : message})
+//    /* 서버로 send 이벤트 전달(데이터포함)*/
+//}

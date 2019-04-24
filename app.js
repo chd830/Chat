0cf088b71d690c66a7b8b4ce58ed407ca7f19d24
chong <chd830@naver.com>
2019-04-24 16:14:48 +0900

    ejs사용하기 더 수정필요

--- app.js
+++ app.js
@@ -53,76 +53,72 @@
 ///* listen메소드로 서버 시작하기 */
 ///* response.send로 데이터 보내기 */
 
-
+////namespace안에 room 두 개 설정하기
 //const app = require('express')();
-//
-//const server = require('http').Server(app);
-//
+//const http = require('http').Server(app);
 //const io = require('socket.io')(server);
 //
-//const fs = require('fs');
-//
-////io.on('connection', (socket) => {
-////    console.log('a user connected');
-////    
-////    socket.on('chat message', (msg) => {
-////        io.emit('chat message', msg);
-////    });
-////    socket.on('disconnect', () => {
-////        console.log('user disconnected');
-////    });
-////});
-//
-//app.get('/', function(request, response) {
-//    fs.readFile('./static/index.html', function(err, data) {
-//        if(err) {
-//            response.send('error')
-//        } else {
-//            response.writeHead(200, { 'Content-Type': 'text/html' })
-//            response.write(data)
-//            response.end()
-//        }
-//    })
-//});
-//server.listen(8080, () => {
-//    console.log('Connected at 8080');
+//server.listen(3000, () => {
+//  console.log('connect 3000');
 //});
 //
+//app.get('/', (req, res) => {
+//  res.sendFile(__dirname + '/static/index.html');
+//});
+//
+//// NameSpace 1번
 //const namespace1 = io.of('/namespace1');
 //
+//// connection을 받으면, news 이벤트에 hello 객체를 담아 보낸다
 //namespace1.on('connection', (socket) => {
-//    namespace1.emit('new', {hello: "someone connected at namespace1"});
+//  namespace1.emit('news', { hello: "Someone connected at namespace1" });
 //});
 //
-//const namespace2 = io.of('/namespace2');
+//// NameSpace 2번
+//const namespace2= io.of('/namespace2');
 //
+//// connection을 받으면, news 이벤트에 hello 객체를 담아 보낸다
 //namespace2.on('connection', (socket) => {
-//    namespace2.emit('new', {hello: "someone connected at namespace2"});
+//  namespace1.emit('news', { hello: "Someone connected at namespace2" });
 //});
+
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-server.listen(3000, () => {
-  console.log('connect 3000');
-});
+app.set('view engine', 'ejs');
+app.set('views', '/views');
+
+let room = ['room1', 'room2'];
+let a = 0;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/static/index.html');
+    res.render('chat');
 });
 
-// NameSpace 1번
-const namespace1 = io.of('/namespace1');
-
-// connection을 받으면, news 이벤트에 hello 객체를 담아 보낸다
-namespace1.on('connection', (socket) => {
-  namespace1.emit('news', { hello: "Someone connected at namespace1" });
+io.on('connection', (socket) => {
+      socket.on('disconnect', () => {
+          console.log('user disconnected');
+      });
+    socket.on('leaveRoom', (num, name) => {
+        socket.leave(room[num], () => {
+            console.log(name + ' leave a ' + room[num]);
+            io.to(room[num]).emit('leaveRoom', num, name);
+        });
+    });
+    socket.on('joinRoom', (num, name) => {
+        socket.join(room[num], () => {
+            console.log(name + ' join a ' + room[name]);
+            io.to(room[num]).emit('joinRoom', num, name);
+        });
+    });
+    
+    socket.on('chat message', (num, name, msg) => {
+        a = num;
+        io.to(room  [a]).emit('chat message', name, msg);
+    });
 });
 
-// NameSpace 2번
-const namespace2= io.of('/namespace2');
-
-// connection을 받으면, news 이벤트에 hello 객체를 담아 보낸다
-namespace2.on('connection', (socket) => {
-  namespace1.emit('news', { hello: "Someone connected at namespace2" });
-});
+server.listen(3000, () => {
+    console.log('connect at 3000');
+})
